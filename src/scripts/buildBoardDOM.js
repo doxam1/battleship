f38c5e1c9ee6ba0f18aefdc125034fd04195f28a
Doxam1 <143098976+doxam1@users.noreply.github.com>
2024-08-31 13:29:51 +0300

    build board fn class added with numbers of cells

--- src/scripts/buildBoardDOM.js
+++ src/scripts/buildBoardDOM.js
@@ -5,9 +5,21 @@ export default function buildBoard(squareXsqaure, boardClass) {
 
   for (let i = 0; i < squareXsqaure * squareXsqaure; i++) {
     const square = document.createElement('div');
-    square.classList.add('square');
+
+
+    // numbering the cells. - added this so line 0 will have two digits. 
+    let cellNum;
+    if (i < 10) {
+      cellNum = '0' + `${i}`
+    } else {
+      cellNum = i;
+    }
+
+    square.classList.add('square', `${boardClass}`, `${cellNum.toString().split('').join(':')}`);
     square.style.width = (400 / squareXsqaure) + 'px';
     square.style.height = (400 / squareXsqaure) + 'px';
     board.appendChild(square);
   }
 }
+
+//i.toString().split('').join(':')
